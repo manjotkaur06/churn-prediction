3bd358616f48bfe177d96a639d5634e1660a2dad
Manjot Kaur <jotkaur22358@gmail.com>
2026-04-14 12:20:56 +0530

    ready for deployment

--- app/app.py
+++ app/app.py
@@ -48,4 +48,7 @@ def predict():
     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
